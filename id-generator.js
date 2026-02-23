5e52c5429789ebb046c26873302d2697fa6f3374
chrinore Esambu <chrinoren@gmail.com>
2026-02-23 02:52:08 +0100

    Update id-generator.js

--- id-generator.js
+++ id-generator.js
@@ -1,4 +1,4 @@
-const date=new Date().getFullYear();
-const suffix = "Maj";
-const number = Math.floor(Math.random()*100)+1
+const date: number=new Date().getFullYear();
+const suffix: string = "Maj";
+const number: number = Math.floor(Math.random()*100)+1
 console.log(`${date}${suffix}${number}`);
